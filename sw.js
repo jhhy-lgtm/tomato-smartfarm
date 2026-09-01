84dd7f619ab09c1fe6d92312a31c0c3b252db30c
jhhy-lgtm <jhhyswsa@gmail.com>
2026-09-01 14:41:45 +0900

    Upload sw.js via Antigravity

---
+++ sw.js
@@ -0,0 +1,36 @@
+﻿const CACHE_NAME = 'tomato-farm-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(() => {});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
